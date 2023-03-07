1557b8bf6d829469ce941572f0630b7e964868ef
sbmishkat <118377434+sbmishkat@users.noreply.github.com>
2023-03-07 18:12:56 +0600

    Update Design_Tutorial_Learn_from_Math.java

--- Design_Tutorial_Learn_from_Math.java
+++ Design_Tutorial_Learn_from_Math.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class DesignTutorialLearnfromMath {
+public class Design_Tutorial_Learn_from_Math {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int a =sc.nextInt();

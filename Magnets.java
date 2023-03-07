738b46795e42c4d310b73654a615d8adb699f7d5
sbmishkat <118377434+sbmishkat@users.noreply.github.com>
2023-03-07 18:17:28 +0600

    Update Magnets.java

--- Magnets.java
+++ Magnets.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class Magnet {
+public class Magnets {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int count = 1, n = sc.nextInt();

754d17914be4fcd526c3c992d2a9d9aaf0849233
sbmishkat <118377434+sbmishkat@users.noreply.github.com>
2023-03-07 18:09:09 +0600

    Update Anton_and_Polyhedrons.java

--- Anton_and_Polyhedrons.java
+++ Anton_and_Polyhedrons.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class AntonandPolyhedrons {
+public class Anton_and_Polyhedrons {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int sum = 0, n = Integer.parseInt(sc.nextLine());

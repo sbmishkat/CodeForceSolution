7144ffa784355648dc8f3dcd87043e6e2a3a6152
sbmishkat <118377434+sbmishkat@users.noreply.github.com>
2023-03-07 18:09:31 +0600

    Update Arrival_of_the_General.java

--- Arrival_of_the_General.java
+++ Arrival_of_the_General.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class ArrivaloftheGeneral {
+public class Arrival_of_the_General {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();

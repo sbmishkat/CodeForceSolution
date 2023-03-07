1342f24d5d3aa717c3a2530ebc2d78f16d801eef
sbmishkat <118377434+sbmishkat@users.noreply.github.com>
2023-03-07 18:21:23 +0600

    Update Soft_Drinking.java

--- Soft_Drinking.java
+++ Soft_Drinking.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class SoftDrinking {
+public class Soft_Drinking {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int[] arr = new int[8];

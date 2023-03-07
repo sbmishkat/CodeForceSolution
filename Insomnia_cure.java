32fd24751b996846bbffb899167178b02348d276
sbmishkat <118377434+sbmishkat@users.noreply.github.com>
2023-03-07 18:16:37 +0600

    Update Insomnia_cure.java

--- Insomnia_cure.java
+++ Insomnia_cure.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Insomniacure {
+public class Insomnia_cure {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();

43f0cfe592f569372bf38517f36a399ecd8e5c4c
sbmishkat <118377434+sbmishkat@users.noreply.github.com>
2023-03-07 18:23:15 +0600

    Update Ultra_Fast_Mathematician.java

--- Ultra_Fast_Mathematician.java
+++ Ultra_Fast_Mathematician.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class UltraFastMathematician {
+public class Ultra_Fast_Mathematician {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String n1 = sc.nextLine();

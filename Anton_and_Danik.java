c81ffcfcf00dbfaa9f1320b0777f021b28bcf656
sbmishkat <118377434+sbmishkat@users.noreply.github.com>
2023-03-07 18:47:09 +0600

    change

--- Anton_and_Danik.java
+++ Anton_and_Danik.java
@@ -1,21 +1,23 @@
 import java.util.*;
-public class Anton_and_Danik {
-    public static void main(String[] args){
-        Scanner scanner= new Scanner(System.in);
-        int acount=0,dcount=0,times =Integer.parseInt(scanner.nextLine());
-        String won = scanner.nextLine();
 
-        for(int i=0;i<times;i++){
-            if(won.charAt(i)=='A')
+public class Anton_and_Danik {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int acount = 0, dcount = 0, times = Integer.parseInt(sc.nextLine());
+        String won = sc.nextLine();
+
+        for (int i = 0; i < times; i++) {
+            if (won.charAt(i) == 'A')
                 acount++;
             else
                 dcount++;
         }
-        if(acount==dcount)
+        if (acount == dcount)
             System.out.println("Friendship");
-        else if (acount>dcount) {
+        else if (acount > dcount) {
             System.out.println("Anton");
-        }else
+        } else
             System.out.println("Danik");
+        sc.close();
     }
 }

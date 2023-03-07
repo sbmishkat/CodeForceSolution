6e450c82354c1963c8b1e7668decedd9646211e9
sbmishkat <118377434+sbmishkat@users.noreply.github.com>
2023-03-07 18:08:15 +0600

    Update Anton_and_Letters.java

--- Anton_and_Letters.java
+++ Anton_and_Letters.java
@@ -1,7 +1,7 @@
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class AntonandLetters {
+public class Anton_and_Letters {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int count = 1;

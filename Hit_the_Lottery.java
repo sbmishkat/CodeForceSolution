d3c4d28b1c584bc75b02fc2fb0998818851b1b5d
sbmishkat <118377434+sbmishkat@users.noreply.github.com>
2023-03-07 18:15:51 +0600

    Update Hit_the_Lottery.java

--- Hit_the_Lottery.java
+++ Hit_the_Lottery.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class HittheLottery {
+public class Hit_the_Lottery {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();

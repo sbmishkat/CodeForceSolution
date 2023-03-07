95d95023ea63d469dc9b25e1f0a7bd6b4acef5ed
sbmishkat <118377434+sbmishkat@users.noreply.github.com>
2023-03-07 18:22:50 +0600

    Update The_New_Year_Meeting_Friends.java

--- The_New_Year_Meeting_Friends.java
+++ The_New_Year_Meeting_Friends.java
@@ -1,7 +1,7 @@
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class TheNewYearMeetingFriends {
+public class The_New_Year_Meeting_Friends {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int[] arr = new int[3];

3318fd1b15d0763e823c5c7acced814b85d35c9f
sbmishkat <118377434+sbmishkat@users.noreply.github.com>
2023-03-07 18:17:07 +0600

    Update Is_your_horseshoe_on_the_other_hoof.java

--- Is_your_horseshoe_on_the_other_hoof.java
+++ Is_your_horseshoe_on_the_other_hoof.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class Isyourhorseshoeontheotherhoof {
+public class Is_your_horseshoe_on_the_other_hoof {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         long[] arr = new long[4];

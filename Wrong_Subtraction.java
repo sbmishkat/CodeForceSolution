92aa2f25310923c83ea1138d336f5c2e7572cdac
sbmishkat <118377434+sbmishkat@users.noreply.github.com>
2023-03-07 18:25:44 +0600

    Update Wrong_Subtraction.java

--- Wrong_Subtraction.java
+++ Wrong_Subtraction.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class WrongSubtraction {
+public class Wrong_Subtraction {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 

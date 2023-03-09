a6c569737e37913224973a753d39c70e01b69afe
sbmishkat <118377434+sbmishkat@users.noreply.github.com>
2023-03-09 20:57:16 +0600

    a

--- Next_Round.java
+++ Next_Round.java
@@ -1,25 +1,20 @@
 import java.util.Scanner;
 
-public class Next_Round
-{
-    public static void main(String[] args)
-    {
+public class Next_Round {
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int nextRound=0,numPlayer,scoreCom;
+        int nextRound = 0, numPlayer, scoreCom;
         numPlayer = scanner.nextInt();
         scoreCom = scanner.nextInt();
 
         int[] score = new int[numPlayer];
 
-        for(int i=0;i<numPlayer;i++)
-        {
-            score[i]=scanner.nextInt();
+        for (int i = 0; i < numPlayer; i++) {
+            score[i] = scanner.nextInt();
         }
-        for(int i=0;i<numPlayer;i++)
-        {
-            if(score[i]>0)
-            {
-                if(score[i]>=score[scoreCom-1])
+        for (int i = 0; i < numPlayer; i++) {
+            if (score[i] > 0) {
+                if (score[i] >= score[scoreCom - 1])
                     nextRound++;
             }
         }
